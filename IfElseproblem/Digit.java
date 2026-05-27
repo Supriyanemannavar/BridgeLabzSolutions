e6790e0997d2dbee5e61604f49f8eb4f79ea88c9
Supriyanemannavar <supriyanemannavar28@gmail.com>
2026-05-27 20:22:19 +0530

    changes in the code

--- IfElseproblem/Digit.java
+++ IfElseproblem/Digit.java
@@ -2,6 +2,7 @@ package IfElseproblem;
 import java.util.*;
 
 public class Digit {
+
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
@@ -9,51 +10,51 @@ public class Digit {
         System.out.print("Enter a single digit number: ");
         int num = sc.nextInt();
 
-        switch(num) {
-
-            case 0:System.out.println("Zero");
-                   break;
-                
-
-            case 1:System.out.println("One");
-                   break;
-                
-
-            case 2: System.out.println("Two");
-                    break;
-               
-
-            case 3: System.out.println("Three");
-                    break;
-               
-
-            case 4:System.out.println("Four");
-                   break;
-                
-
-            case 5:System.out.println("Five");
-                   break;
-                
-
-            case 6:System.out.println("Six");
-                   break;
-                
-
-            case 7:System.out.println("Seven");
-                  break;
-                
-
-            case 8:System.out.println("Eight");
-                   break;
-                
-
-            case 9:System.out.println("Nine");
-                   break;
-                
-
-            default:System.out.println("Invalid Input");
-                
+        if(num == 0) {
+            System.out.println("Zero");
         }
 
-    
-}}
+        else if(num == 1) {
+            System.out.println("One");
+        }
+
+        else if(num == 2) {
+            System.out.println("Two");
+        }
+
+        else if(num == 3) {
+            System.out.println("Three");
+        }
+
+        else if(num == 4) {
+            System.out.println("Four");
+        }
+
+        else if(num == 5) {
+            System.out.println("Five");
+        }
+
+        else if(num == 6) {
+            System.out.println("Six");
+        }
+
+        else if(num == 7) {
+            System.out.println("Seven");
+        }
+
+        else if(num == 8) {
+            System.out.println("Eight");
+        }
+
+        else if(num == 9) {
+            System.out.println("Nine");
+        }
+
+        else {
+            System.out.println("Invalid Input");
+        }
+
+        sc.close();
+    }
+}
+
--- IfElseproblem/UnitTenHundred.java
+++ IfElseproblem/UnitTenHundred.java
@@ -2,61 +2,50 @@ package IfElseproblem;
 
 public class UnitTenHundred {
     public static void main(String args[]){
-        int n=10000123;
+        int n = 10000123;
+        int count = 0;
 
-        int count=0;
-
-        while(n>0&&count<3){
-            int num=n%10;
+        while (n > 0 && count < 3) {
+            int num = n % 10;
             count++;
-            n=n/10;
-
-            switch(num) {
-
-            case 0:System.out.println("Zero");
-                   break;
-                
-
-            case 1:System.out.println("One");
-                   break;
-                
-
-            case 2: System.out.println("Two");
-                    break;
-               
-
-            case 3: System.out.println("Three");
-                    break;
-               
-
-            case 4:System.out.println("Four");
-                   break;
-                
-
-            case 5:System.out.println("Five");
-                   break;
-                
-
-            case 6:System.out.println("Six");
-                   break;
-                
-
-            case 7:System.out.println("Seven");
-                  break;
-                
-
-            case 8:System.out.println("Eight");
-                   break;
-                
-
-            case 9:System.out.println("Nine");
-                   break;
-                
-
-            default:System.out.println("Invalid Input");
-                
-        }
+            n = n / 10;
+        if (n == 0) {
+                System.out.println("Zero");
+            } 
+            else if (n== 1) {
+                System.out.println("One");
+            } 
+            else if (n == 2) {
+                System.out.println("Two");
+            } 
+            else if (n== 3) {
+                System.out.println("Three");
+            } 
+            else if (n== 4) {
+                System.out.println("Four");
+            } 
+            else if (n== 5) {
+                System.out.println("Five");
+            } 
+            else if (n== 6) {
+                System.out.println("Six");
+            } 
+            else if (n== 7) {
+                System.out.println("Seven");
+            } 
+            else if (n == 8) {
+                System.out.println("Eight");
+            } 
+            else if (n== 9) {
+                System.out.println("Nine");
+            } 
+            else {
+                System.out.println("Invalid Input");
+            }
         }
     }
-    
+
 }
+                
+       
+
