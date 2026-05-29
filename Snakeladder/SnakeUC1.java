ae820b691916b0fcd044b1953fd278984a49cbc3
Supriyanemannavar <supriyanemannavar28@gmail.com>
2026-05-29 18:59:07 +0530

    Changes in the code

--- Snakeladder/SnakeUC1.java
+++ Snakeladder/SnakeUC1.java
@@ -1,6 +1,15 @@
 package Snakeladder;
 
 public class SnakeUC1 {
-    public static int START_POSITION=0;
+    public static void main(String[] args) {
+
+        int position = 0;
+
+        int dice = (int)(Math.floor(Math.random() * 6) + 1);
+
+        System.out.println("Starting Position: " + position);
+
+        System.out.println("Dice Number: " + dice);
+    }
     
 }
