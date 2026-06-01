a7ba5b500f09a61f0b02aa6ffee7c163f8e254eb
Supriyanemannavar <supriyanemannavar28@gmail.com>
2026-06-01 10:44:19 +0530

    First Commit

---
+++ Gambling/GamblerUC1.java
@@ -0,0 +1,14 @@
+package Gambling;
+
+public class GamblerUC1 {
+    public static void main(String[] args) {
+
+        int stake = 100;
+        int bet = 1;
+
+        System.out.println("Stake = $" + stake);
+        System.out.println("Bet = $" + bet);
+    }
+
+    
+}
