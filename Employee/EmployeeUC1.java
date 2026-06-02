025a409cd6bc3a4bcfcdf571db3f2e3af6f53c1d
Supriyanemannavar <supriyanemannavar28@gmail.com>
2026-06-02 11:37:19 +0530

    Welcome message

---
+++ Employee/EmployeeUC1.java
@@ -0,0 +1,8 @@
+package Employee;
+
+public class EmployeeUC1 {
+    public static void main(String args[]){
+        System.out.println("Welcome to Employee weg Computation Program");
+    }
+    
+}
