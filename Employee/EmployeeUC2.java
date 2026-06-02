4014b805b5384a47bb6c1cf935ab6a81abc92f9d
Supriyanemannavar <supriyanemannavar28@gmail.com>
2026-06-02 11:47:52 +0530

    Employee presence

---
+++ Employee/EmployeeUC2.java
@@ -0,0 +1,19 @@
+package Employee;
+
+public class EmployeeUC2 {
+    public static void main(String args[]){
+        int present=1;
+
+        if(present==(int)Math.random()*2){
+            System.out.println("Employee is present ");
+        }
+        else{
+            System.out.println("Employee is Absent");
+        }
+
+
+
+
+    }
+    
+}
