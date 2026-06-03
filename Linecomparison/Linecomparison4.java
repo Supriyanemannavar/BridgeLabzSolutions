495cc2cdf0782268e8ac7ee0f6ad11d249d03c9a
Supriyanemannavar <supriyanemannavar28@gmail.com>
2026-06-03 14:11:31 +0530

    First Commit

---
+++ Linecomparison/Linecomparison4.java
@@ -0,0 +1,22 @@
+package Linecomparison;
+
+public class Linecomparison4 {
+    public static void main(String[] args) {
+
+        Double line1 = 5.6;
+        Double line2 = 7.2;
+
+        int result = line1.compareTo(line2);
+
+        if (result == 0) {
+            System.out.println("Lines are Equal");
+        }
+        else if (result > 0) {
+            System.out.println("Line 1 is Greater");
+        }
+        else {
+            System.out.println("Line 2 is Greater");
+        }
+    }
+    
+}
