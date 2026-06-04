6e5169794cbed571f0acc79d4a0cd66692ee33ac
Supriyanemannavar <supriyanemannavar28@gmail.com>
2026-06-04 12:05:19 +0530

    changes in the code

--- EmployeePayRolls/Emp1.java
+++
@@ -1,8 +0,0 @@
-package EmployeePayRolls;
-
-public class Emp1 {
-     public static void main(String args[]){
-        System.out.println("Welcome to Employee weg Computation Program");
-    }
-    
-}
