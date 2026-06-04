0c14c56370bde8e7774c0889f8654d47bb0a2465
Supriyanemannavar <supriyanemannavar28@gmail.com>
2026-06-04 11:30:28 +0530

    Initial commit

---
+++ EmployeePayRolls/Emp2.java
@@ -0,0 +1,19 @@
+package EmployeePayRolls;
+
+public class Emp2 {
+    public static void main(String args[]){
+        int present=1;
+
+        if(present==(int)Math.random()*2){
+            System.out.println("Employee is present ");
+        }
+        else{
+            System.out.println("Employee is Absent");
+        }
+
+
+
+
+    }
+    
+}
