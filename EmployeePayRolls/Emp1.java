0c2d1ce3e7d84e7736a2c1149ae75f6307416d1a
Supriyanemannavar <supriyanemannavar28@gmail.com>
2026-06-04 11:28:25 +0530

    Initial commit

---
+++ EmployeePayRolls/Emp1.java
@@ -0,0 +1,8 @@
+package EmployeePayRolls;
+
+public class Emp1 {
+     public static void main(String args[]){
+        System.out.println("Welcome to Employee weg Computation Program");
+    }
+    
+}
